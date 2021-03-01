03b97ceca47a0b05b61fc84895956db39e0bd0d3
Ivan Arakcheev <wildfiler0@gmail.com>
2021-03-01 22:03:44 +0200

    Add docs to Properties class

--- lib/tiled/properties.rb
+++ lib/tiled/properties.rb
@@ -1,18 +1,37 @@
 module Tiled
   class Properties
+    #  Class for storing custom properties of Tiled map elements.
+
     include Tiled::Serializable
 
     def initialize(map)
       @map = map
     end
 
-    def add(name, type, value)
+    # Creates method for accessing new property by name. For boolean type adds `?` method
+    #
+    # @example
+    #   prop = Tiled::Properties.new(map)
+    #   prop.add(:visible, 'bool', 'true')
+    #   prop.visible? # => true
+    #
+    # @param name [String, Symbol] name of new property
+    # @param type [String, nil] type of property, `string` by default
+    # @param value [Integer, Float, Boolean, String] value of added property
+    # @return [Tiled::Properties] self
+    def add(name, type = 'string', value)
       instance_variable_set("@#{name}", value)
       instance_variable_set("@#{name}_type", type)
       define_singleton_method(name) { instance_variable_get(:"@#{name}") }
-      define_singleton_method("@#{name}_type") { instance_variable_get(:"@#{name}_type") }
+      define_singleton_method("#{name}_type") { instance_variable_get(:"@#{name}_type") }
+      define_singleton_method("#{name}?") { !!instance_variable_get(:"@#{name}") } if type == 'bool'
+
+      self
     end
 
+    # Initialize properties with data from xml hash
+    # @param hash [Hash] hash loaded from xml file of map.
+    # @return [Tiled::Property] self
     def from_xml_hash(hash)
       hash.each do |prop|
         next unless prop[:name] == 'property'
@@ -23,6 +42,8 @@ module Tiled
 
         add(attributes['name'], type, value)
       end
+
+      self
     end
 
     private
