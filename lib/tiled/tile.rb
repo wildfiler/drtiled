179e56c65301612de5eb82673185aa852b65d3ea
Ivan Arakcheev <wildfiler0@gmail.com>
2021-05-27 14:36:50 +0300

    Rework Tiled::Tile#from_xml_hash

--- lib/tiled/tile.rb
+++ lib/tiled/tile.rb
@@ -13,28 +13,23 @@ module Tiled
     # TODO: Add terrain and animation support
     def from_xml_hash(hash)
       attributes.add(hash[:attributes])
-      if tileset.image
-        @path = tileset.image.path
-        @tile_x, @tile_y, @tile_w, @tile_h = tileset.id_to_xywh(id.to_i)
-      else
-        # Tileset doesn't have an image but is made of individual images; we can process those directly
-        image_attributes = hash[:children].select do |item|
-          item[:name] == "image"
-        end.first
-        img = Tiled::Image.new(tileset)
-        img.from_xml_hash(image_attributes)
-        @path = img.path
-        @tile_x = 0
-        @tile_y = 0
-        @tile_w = img.width
-        @tile_h = img.height
-      end
+
       hash[:children].each do |child|
         case child[:name]
         when 'properties'
           properties.from_xml_hash(child[:children])
+        when 'image'
+          img = parse_image(child)
+
+          @path = img.path
+          @tile_x = 0
+          @tile_y = 0
+          @tile_w = img.width
+          @tile_h = img.height
         end
       end
+
+      init_from_tileset unless @path
     end
 
     def init_empty(id)
@@ -52,6 +47,19 @@ module Tiled
 
     def properties
       @properties ||= Properties.new(self)
+    end
+
+    private
+
+    def parse_image(child)
+      Tiled::Image.new(tileset).tap do |img|
+        img.from_xml_hash(child)
+      end
+    end
+
+    def init_from_tileset
+      @path = tileset.image.path
+      @tile_x, @tile_y, @tile_w, @tile_h = tileset.id_to_xywh(id.to_i)
     end
   end
 end
