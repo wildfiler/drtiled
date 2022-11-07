1c8cb87e8ce9c34971d8b0fd78fc221726dfa218
Pete Favelle <pete@fsquared.co.uk>
2022-11-07 14:05:04 +0000

    Fix to handling .. in paths

--- lib/tiled/utils.rb
+++ lib/tiled/utils.rb
@@ -3,7 +3,7 @@ module Tiled
     module_function def relative_to_absolute(path)
       absolute_path = path.split(File::SEPARATOR).inject([]) do |memo, element|
         if element == ".."
-          memo.shift
+          memo.pop
         else
           memo.push(element)
         end
