ee5fdfb2c60768213f885608f1ce65fe667c13c3
vinnydiehl <vinny.diehl@gmail.com>
2023-04-30 03:45:18 -0400

    Render fill for polygon objects
    I overcame the performance impact of polygons by rendering them to
    sprites, and then simply rendering those sprites in the correct
    position.

--- lib/tiled/object_layer.rb
+++ lib/tiled/object_layer.rb
@@ -79,19 +79,96 @@ module Tiled
             **color.to_h, a: color.a * 0.7
           }
         when :polygon
-          # Get the starting point of the polygon
-          offset = [object.x + object.points[0].x, object.y + object.points[0].y]
+          target = :"polygon#{object.id}"
 
-          # Draw the outline connecting each point
-          object.points.each_with_index do |point, index|
-            next_point = object.points[(index + 1) % object.points.length]
+          # Find the top and bottom of the polygon
+          y_values = object.points.map { |p| p.y }
+          min_y, max_y = y_values.min, y_values.max
+          # Find the sides
+          x_values = object.points.map { |p| p.x }
+          min_x, max_x = x_values.min, x_values.max
 
-            outputs_layer << {
-              x: offset.x + point.x, y: offset.y + point.y,
-              x2: offset.x + next_point.x, y2: offset.y + next_point.y,
-              **color.to_h
-            }
+          args.state.polygon_cached ||= {}
+          unless args.state.polygon_cached[target]
+            # Get the starting point of the polygon
+            offset = [object.points[0].x * 2 - min_x + 1, object.points[0].y * 2 - min_y + 1]
+
+            # Similar to the circle, this is drawn as a bunch of horizontal lines
+            (max_y - min_y).to_i.times do |y|
+              # We need to get the intersections where a horizontal line
+              # across the screen crosses the edges of the polygon
+              intersections = []
+
+              y += min_y
+
+              object.points.each_with_index do |point, index|
+                next_point = object.points[(index + 1) % object.points.length]
+
+                # We're iterating over each line of the polygon. This if statement
+                # will hit on each line that intersects the line that we're drawing
+                if (point.y <= y && next_point.y > y) || (next_point.y <= y && point.y > y)
+                  if point.y == next_point.y
+                    # The edge is horizontal, so the intersection is just the
+                    # X-coordinate of the point
+                    intersections << offset.x + point.x
+                  else
+                    # Find the X-coordinate where the edge intersects the
+                    # row using the equation of the line
+                    intersections << offset.x +
+                      ((y - point.y) * (next_point.x - point.x) /
+                       (next_point.y - point.y)) + point.x
+                  end
+                end
+              end
+
+              # Y-coordinate on the sprite that this line is being drawn
+              sprite_y = offset.y + y
+
+              # `intersections` contains every X coordinate where the line that we're drawing
+              # crosses the border of the shape we need to draw. In cases like a triangle, there
+              # will only be 2 intersections and we can just draw the line between them. But for
+              # more complex shapes where there e.g. a bunch of vertical spikes, we will have an
+              # (always evenly sized) array that we will need to sort and then iterate over
+              # 2 at a time, drawing lines between each slice of 2.
+              intersections.sort! if intersections.size > 2
+
+              i = 0
+              while i < intersections.length - 1 do
+                args.render_target(target).lines << {
+                  x: intersections[i], y: sprite_y,
+                  x2: intersections[i + 1], y2: sprite_y,
+                  **color.to_h, a: color.a * 0.7
+                }
+                i += 2
+              end
+            end
+
+            # Draw the outline connecting each point
+            object.points.each_with_index do |point, index|
+              next_point = object.points[(index + 1) % object.points.length]
+
+              args.render_target(target).lines << {
+                x: offset.x + point.x, y: offset.y + point.y,
+                x2: offset.x + next_point.x, y2: offset.y + next_point.y,
+                **color.to_h
+              }
+            end
+
+            args.state.polygon_cached[target] = true
           end
+
+          width = max_x - min_x + 2
+          height = max_y - min_y + 2
+
+          outputs_layer << {
+            x: object.x - (object.points[0].x - min_x) - 1,
+            y: object.y - (object.points[0].y - min_y) - 1,
+            w: width, h: height,
+            path: target,
+            source_x: 0, source_y: 0,
+            source_w: width, source_h: height,
+            **color.to_h
+          }
         when :point
           size = 10
           outputs_layer << {
